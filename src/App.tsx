b4e3faae07365f75c0137bc968c6d0001d5c9229
WillG_o6 <giles.william17@icloud.com>
2026-05-03 11:26:29 +0100

    preparing for domain

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Gilevo-Co/">
+      <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
